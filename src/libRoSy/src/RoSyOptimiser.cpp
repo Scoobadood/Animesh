5dc2f638680ac5fe938ac47f02536ca9c4151602
Dave <dave.d@canva.com>
2021-06-05 15:31:05 +1000

    Randomise the node processing order if set in properties file

--- src/libRoSy/src/RoSyOptimiser.cpp
+++ src/libRoSy/src/RoSyOptimiser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2bccd8da98d29dbea9d66518637daa63aae63e12580744bf11529fc6474f116
-size 5484
+oid sha256:bf6a55e3484efc7ca7c00fba348f2a0d5a18a37f69b1821558a278548ea946b8
+size 5820

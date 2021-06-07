3271594be0facd42ec2c6f754e6ea35155402fa7
Dave <dave.d@canva.com>
2021-06-07 14:56:37 +1000

    Allow randomised neighbours in PoSy smooother

--- src/libPoSy/src/PoSyOptimiser.cpp
+++ src/libPoSy/src/PoSyOptimiser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9afbd8aa04187d87b19549585b3765656994dd4bba2ae92f63fdf6a81333ac40
-size 9426
+oid sha256:556dd8177f36d167a9470f7e3100ddf667912b06333ff9ed56dd3c4327a0fab9
+size 9526

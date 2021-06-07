022bce2bb18235b61ae63da145eeff3f567e78bb
Dave <dave.d@canva.com>
2021-06-07 18:06:40 +1000

    Made lines thicker and removed graph paperiness

--- src/posy_visualiser/posy_gl_widget.cpp
+++ src/posy_visualiser/posy_gl_widget.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d82918a5b71850d875e9cbb1a6cb5088c44a3c45924a7eba74619d9dc357f7a7
-size 9932
+oid sha256:77a3f9f67d6128a9591aaf8042dc63edc740dade689e618b58adf660e33a66b1
+size 9821

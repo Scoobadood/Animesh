9b5d29238d0872a166a69f9e58d7c35f42372f1b
Dave <dave.d@canva.com>
2021-06-27 14:33:25 +1000

    Sdded OBJ fil handling

--- src/libGeomFileUtils/src/ObjFileParser.cpp
+++ src/libGeomFileUtils/src/ObjFileParser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:986c61c77bdc739404c194dda78644fffe553f17ac7a44e805286e2d6e445e51
-size 11910
+oid sha256:b97c6662d0ef4428d846d8ce14576c368fe77c59d22211d37efd60a337679c47
+size 13750

d614efb3d0f3fa005be2b1c12b0ce3edf367b6e2
Dave <dave.d@canva.com>
2021-06-28 13:45:31 +1000

    Fixed Obj file parser bug

--- src/libGeomFileUtils/src/ObjFileParser.cpp
+++ src/libGeomFileUtils/src/ObjFileParser.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b97c6662d0ef4428d846d8ce14576c368fe77c59d22211d37efd60a337679c47
-size 13750
+oid sha256:84768c418d9a937d353650c9330b5412ac7ba6e5779ea675ffd4bb636ce970ae
+size 12861

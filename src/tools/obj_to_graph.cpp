9a3518090549d19dee1370e6bde8fe74b3863e39
Dave <dave.d@canva.com>
2021-07-12 15:30:40 +1000

    Quadluator now saves ASCII or Bin PLY files and is working

--- src/tools/obj_to_graph.cpp
+++ src/tools/obj_to_graph.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d45f9f797cbde60c4c54d6e3c475bdd1af75dd2d004e1f4af7002e1713a1f70
-size 1877
+oid sha256:f51d9169c9513acf4f6a115888d2d09c2c5990da3f7f0fcf94b8903970d9524a
+size 1923
--- src/tools/quadulator/main.cpp
+++ src/tools/quadulator/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f44a97f96b3cc0a621c799c9ed33478c1d99b60cdfdda5558cec66ef602247b0
-size 2860
+oid sha256:d589adf027d46d89e4619e81038c6e81402de42c69622c7025ff176ca19814e0
+size 8863

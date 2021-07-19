ad23d1ce7fb31f157bab5b67047899ec1db16006
Dave <dave.d@canva.com>
2021-07-19 23:16:50 +1000

    Adjusted logging levels

--- src/libSurfel/src/Surfel_IO.cpp
+++ src/libSurfel/src/Surfel_IO.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd2fbb58e8d86e7721473ed69401daa2a26607bb1c69f50b3ee6fd75b68edeb4
-size 10153
+oid sha256:794798e1fe6ba1619f25726cb3788e97e655e851b951268250e74a77649cde43
+size 10154

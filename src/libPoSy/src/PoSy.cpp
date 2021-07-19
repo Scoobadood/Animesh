8c61cb8205d2552522a5cc496d51727d755d74fa
Dave <dave.d@canva.com>
2021-07-19 23:17:30 +1000

    Cosmetic changes to find_closest_points

--- src/libPoSy/src/PoSy.cpp
+++ src/libPoSy/src/PoSy.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31b56bda23d8998a65f6a6e010cca84a26bab0541b591edd6c83518222404571
-size 12423
+oid sha256:06bdbd96e657e91f805606858712c76e7eb42b78bd203c603e79bad17fd2336b
+size 12477

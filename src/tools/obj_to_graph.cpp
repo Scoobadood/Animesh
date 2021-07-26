dd8a176f099015383cf6371874f897b092d5a57a
Dave <dave.d@canva.com>
2021-07-26 19:04:30 +1000

    Externalised scale in obj tool

--- src/tools/obj_to_graph.cpp
+++ src/tools/obj_to_graph.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26f3fd2b9800f32174fc928f35dbca8055134f68953158dc6b9a1227561d6452
-size 1923
+oid sha256:d84ede36318fa4fef9995dfabc33b4e9493a88fe560c0e8da6cd5fa2dcca60b7
+size 2468

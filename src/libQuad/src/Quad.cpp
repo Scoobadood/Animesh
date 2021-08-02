8d9b6ba81e33c081725f1dee7ced311d052a2338
Dave <dave.d@canva.com>
2021-08-02 13:46:33 +1000

    Calculate the PoSy from best RoSy

--- src/libQuad/src/Quad.cpp
+++ src/libQuad/src/Quad.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fecd6802ca664a22ae82a4ed51bab9473567ada475d00baff5835644517f5d4
-size 4573
+oid sha256:98b60b27d15e1ff7f35a32245630c4dce94fc9d43042fdd287c5d301b9a7efb1
+size 4898

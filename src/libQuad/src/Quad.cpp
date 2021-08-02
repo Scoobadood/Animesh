fb69c8ea18678703ed9b0e0699f6aea7b914c45d
Dave <dave.d@canva.com>
2021-08-02 13:05:21 +1000

    Process each edge only once

--- src/libQuad/src/Quad.cpp
+++ src/libQuad/src/Quad.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29151e4f9ba62618f2d553c1b5aff2b77b20b4b99fb266a37abcc001995df1d7
-size 4800
+oid sha256:2fecd6802ca664a22ae82a4ed51bab9473567ada475d00baff5835644517f5d4
+size 4573

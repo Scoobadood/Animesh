75f2eb010ca93851d8992d05ecac2ef12eeaba8f
Dave <dave.d@canva.com>
2021-10-25 17:51:55 +1100

    Minimum compilable multi-res optimiser implemented

--- src/libMultiResolutionOptimiser/include/MultiResolutionOptimiser/MultiResolutionOptimiser.h
+++ src/libMultiResolutionOptimiser/include/MultiResolutionOptimiser/MultiResolutionOptimiser.h
@@ -21,9 +21,20 @@ protected:
   void smoothing_completed(float smoothness, OptimisationResult result) override;
 
 private:
+  const std::string &get_ssa_property_name() const override;
+  const std::string &get_ssa_percentage_property_name() const override;
+
+  void optimise_node(const SurfelGraphNodePtr &node) override;
+  bool compare_worst_first(const SurfelGraphNodePtr &l, const SurfelGraphNodePtr &r) const override;
+  float compute_node_smoothness_for_frame(
+      const SurfelGraphNodePtr &node_ptr,
+      size_t frame_index,
+      unsigned int &num_neighbours) const override;
+  void store_mean_smoothness(SurfelGraphNodePtr node, float smoothness) const override;
+
   MultiResolutionSurfelGraph * m_multi_res_graph;
-  PoSyOptimiser * m_posy_optimiser;
-  RoSyOptimiser * m_rosy_optimiser;
+  std::unique_ptr<PoSyOptimiser> m_posy_optimiser;
+  std::unique_ptr<RoSyOptimiser> m_rosy_optimiser;
   unsigned int m_num_levels;
   unsigned int m_current_level;
 };
--- src/libMultiResolutionOptimiser/src/MultiResolutionOptimiser.cpp
+++ src/libMultiResolutionOptimiser/src/MultiResolutionOptimiser.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "MultiResolutionOptimiser.h"
+
+MultiResolutionOptimiser::MultiResolutionOptimiser(const Properties &properties, std::mt19937 &rng) //
+    : Optimiser{properties, rng} //
+{
+  m_rosy_optimiser = std::make_unique<RoSyOptimiser>(m_properties, rng);
+  m_posy_optimiser = std::make_unique<PoSyOptimiser>(m_properties, rng);
+}
+
+const std::string &
+MultiResolutionOptimiser::get_ssa_property_name() const {
+  spdlog::error("get_ssa_property_name is not yet implemented");
+  static std::string s{"NO SSA PROPERTY NAME"};
+  return s;
+}
+
+const std::string &
+MultiResolutionOptimiser::get_ssa_percentage_property_name() const {
+  spdlog::error("get_ssa_percentage_property_name is not yet implemented");
+  static std::string s{"NO SSA PERCENTAGE PROPERTY NAME"};
+  return s;
+}
+
+void
+MultiResolutionOptimiser::optimise_node(const SurfelGraphNodePtr &node) {
+  spdlog::error("optimise_node is not yet implemented");
+}
+
+bool
+MultiResolutionOptimiser::compare_worst_first(const SurfelGraphNodePtr &l, const SurfelGraphNodePtr &r) const {
+  spdlog::error("compare_worst_first is not yet implemented");
+  return false;
+}
+
+float
+MultiResolutionOptimiser::compute_node_smoothness_for_frame(
+    const SurfelGraphNodePtr &node_ptr,
+    size_t frame_index,
+    unsigned int &num_neighbours) const {
+  spdlog::error("compute_node_smoothness_for_frame is not yet implemented");
+  return std::numeric_limits<float>::infinity();
+}
+
+void
+MultiResolutionOptimiser::store_mean_smoothness(
+    SurfelGraphNodePtr node,
+    float smoothness) const {
+  spdlog::error("store_mean_smoothness is not yet implemented");
+}
+
+void
+MultiResolutionOptimiser::loaded_graph() {
+  spdlog::error("loaded_graph is not yet implemented");
+}
+
+void
+MultiResolutionOptimiser::smoothing_completed(float smoothness, OptimisationResult result) {
+  spdlog::error("smoothing_completed is not yet implemented");
+}
